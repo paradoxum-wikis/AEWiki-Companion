4742e9e9ec162cb5c3e714e821bc3b643d790032
Benjamin <7@toru.ca>
2026-07-24 20:41:24 -0400

    chore(deps): add lucide preprocess

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { sveltekit } from "@sveltejs/kit/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import lucidePreprocess from "vite-plugin-lucide-preprocess";
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()],
+	plugins: [lucidePreprocess(), tailwindcss(), sveltekit()],
 	server: {
 		port: 1405,
 	},
